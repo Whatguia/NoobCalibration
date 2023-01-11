c7c5cef4112749877003463a81dbf9e435d18f04
Hurenzhong <296730738@qq.com>
2023-01-11 17:40:14 +0800

    更改以用于测试

--- script/run_PnP.sh
+++ script/run_PnP.sh
@@ -1,4 +1,5 @@
+points_json_path="./data/points.json"
 intrinsic_json_path="./data/test.json"
-extrinsic_json_path="./data/test.json"
+extrinsic_json_path="./data/test2.json"
 
-./bin/PnP $intrinsic_json_path $extrinsic_json_path
+./bin/PnP $points_json_path $intrinsic_json_path $extrinsic_json_path
