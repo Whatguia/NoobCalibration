9ca6832c7c83644111e1da5a5dbd8198bc82af68
Hrz <64910975+TankGewehr@users.noreply.github.com>
2023-04-01 20:27:50 +0000

    增加根据标定结果将点云投影到图像中的代码

---
+++ script/run_projection.sh
@@ -0,0 +1,5 @@
+image_path="./data/image.jpg"
+pcd_path="./data/pointcloud.pcd"
+intrinsic_and_extrinsic_json_path="./data/calibration.json"
+
+./bin/projection $image_path $pcd_path $intrinsic_and_extrinsic_json_path false
